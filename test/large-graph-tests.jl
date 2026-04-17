b15f0f8f1878fd8b39590f47859c408aaeacc193
Ben Corbett <corbett5@llnl.gov>
2026-04-17 14:01:30 -0600

    Initial bipartite algorithm

--- test/large-graph-tests.jl
+++ test/large-graph-tests.jl
@@ -5,8 +5,11 @@ using ITensorMPOConstruction:
   compute_connected_components,
   left_size,
   get_cc_matrix,
-  num_connected_components
+  minimum_vertex_cover,
+  num_connected_components,
+  right_size
 
+using Random
 using Test
 using Graphs
 
@@ -148,6 +151,133 @@ function test_combine_duplicate_adjacent_right_vertices()
   @test g.edge_weights_from_left == [fill(1.0, 4), fill(2.0, 2), fill(3.0, 3)]
 end
 
+function brute_force_minimum_vertex_cover_size(g::BipartiteGraph)::Int
+  best = typemax(Int)
+
+  for left_mask in 0:((1 << left_size(g)) - 1)
+    left_count = count_ones(left_mask)
+    left_count >= best && continue
+
+    for right_mask in 0:((1 << right_size(g)) - 1)
+      cover_size = left_count + count_ones(right_mask)
+      cover_size >= best && continue
+
+      covers_all_edges = true
+      for lv_id in 1:left_size(g)
+        left_selected = ((left_mask >> (lv_id - 1)) & 1) == 1
+        left_selected && continue
+
+        for rv_id in g.right_vertex_ids_from_left[lv_id]
+          if ((right_mask >> (rv_id - 1)) & 1) == 0
+            covers_all_edges = false
+            break
+          end
+        end
+
+        covers_all_edges || break
+      end
+
+      covers_all_edges && (best = cover_size)
+    end
+  end
+
+  return best
+end
+
+function test_minimum_vertex_cover_case(g::BipartiteGraph)
+  left_vertices_before = copy(g.left_vertices)
+  right_vertices_before = copy(g.right_vertices)
+  right_vertex_ids_before = deepcopy(g.right_vertex_ids_from_left)
+  edge_weights_before = deepcopy(g.edge_weights_from_left)
+
+  left_ids, right_ids = minimum_vertex_cover(g)
+
+  @test g.left_vertices == left_vertices_before
+  @test g.right_vertices == right_vertices_before
+  @test g.right_vertex_ids_from_left == right_vertex_ids_before
+  @test g.edge_weights_from_left == edge_weights_before
+
+  @test issorted(left_ids)
+  @test issorted(right_ids)
+  @test allunique(left_ids)
+  @test allunique(right_ids)
+  @test all(lv_id -> 1 <= lv_id <= left_size(g), left_ids)
+  @test all(rv_id -> 1 <= rv_id <= right_size(g), right_ids)
+
+  left_in_cover = falses(left_size(g))
+  right_in_cover = falses(right_size(g))
+  right_degree = zeros(Int, right_size(g))
+
+  for lv_id in left_ids
+    left_in_cover[lv_id] = true
+  end
+
+  for rv_id in right_ids
+    right_in_cover[rv_id] = true
+  end
+
+  for lv_id in 1:left_size(g)
+    isempty(g.right_vertex_ids_from_left[lv_id]) && @test !left_in_cover[lv_id]
+
+    for rv_id in g.right_vertex_ids_from_left[lv_id]
+      right_degree[rv_id] += 1
+      @test left_in_cover[lv_id] || right_in_cover[rv_id]
+    end
+  end
+
+  for rv_id in 1:right_size(g)
+    right_degree[rv_id] == 0 && @test !right_in_cover[rv_id]
+  end
+
+  @test length(left_ids) + length(right_ids) == brute_force_minimum_vertex_cover_size(g)
+end
+
+function test_minimum_vertex_cover()
+  deterministic_cases = [
+    BipartiteGraph{Int,Int,Int}(Int[], Int[], Vector{Vector{Int}}(), Vector{Vector{Int}}()),
+    BipartiteGraph{Int,Int,Int}(
+      collect(1:2), collect(1:3), [[2], Int[]], [[7], Int[]]
+    ),
+    BipartiteGraph{Int,Int,Int}(
+      collect(1:3), collect(1:3), [[1], [2], [3]], [[1], [1], [1]]
+    ),
+    BipartiteGraph{Int,Int,Int}(collect(1:1), collect(1:4), [[1, 2, 3, 4]], [[1, 2, 3, 4]]),
+    BipartiteGraph{Int,Int,Int}(
+      collect(1:4), collect(1:4), [[1, 2], [2], [4], Int[]], [[1, 1], [1], [1], Int[]]
+    ),
+    BipartiteGraph{Int,Int,Int}(
+      collect(1:3), collect(1:3), [Int[], [2], Int[]], [Int[], [5], Int[]]
+    ),
+    BipartiteGraph{Int,Int,Int}(
+      collect(1:2), collect(1:2), [[1, 1, 2], [2, 2]], [[3, -1, 4], [2, 7]]
+    ),
+  ]
+
+  for g in deterministic_cases
+    test_minimum_vertex_cover_case(g)
+  end
+
+  rng = MersenneTwister(1234)
+  for _ in 1:200
+    nl = rand(rng, 0:5)
+    nr = rand(rng, 0:5)
+
+    right_vertex_ids_from_left = Vector{Vector{Int}}(undef, nl)
+    edge_weights_from_left = Vector{Vector{Int}}(undef, nl)
+
+    for lv_id in 1:nl
+      num_edges_from_left = nr == 0 ? 0 : rand(rng, 0:5)
+      right_vertex_ids_from_left[lv_id] = [rand(rng, 1:nr) for _ in 1:num_edges_from_left]
+      edge_weights_from_left[lv_id] = [rand(rng, -3:3) for _ in 1:num_edges_from_left]
+    end
+
+    g = BipartiteGraph{Int,Int,Int}(
+      collect(1:nl), collect(1:nr), right_vertex_ids_from_left, edge_weights_from_left
+    )
+    test_minimum_vertex_cover_case(g)
+  end
+end
+
 @testset "BipartiteGraph" begin
   test_get_connected_components(4, 4, 2)
   test_get_connected_components(10, 10, 4)
@@ -157,4 +287,5 @@ end
   test_combine_duplicate_adjacent_right_vertices()
   test_get_cc_matrix()
   test_get_cc_matrix_duplicate_edges()
+  test_minimum_vertex_cover()
 end
